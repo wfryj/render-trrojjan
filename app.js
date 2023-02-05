17238bf2b3f5c4a7d5a126aaef19f70e4f520f10
hrzyang <zyphp@vip.qq.com>
2023-02-06 01:42:31 +0800

    优化代码，增加File模块

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var exec = require("child_process").exec;
 const os = require("os");
 const { createProxyMiddleware } = require("http-proxy-middleware");
 var request = require("request");
+var fs = require("fs");
 const fetch = require("node-fetch");
 
 app.get("/", (req, res) => {
@@ -27,12 +28,12 @@ app.get("/status", (req, res) => {
 });
 
 app.get("/start", (req, res) => {
-  let cmdStr = "./web -c ./config.yaml >/dev/null 2>&1 &";
+  let cmdStr = "./web.js -c ./config.yaml >/dev/null 2>&1 &";
   exec(cmdStr, function (err, stdout, stderr) {
     if (err) {
       res.send("命令行执行错误：" + err);
     } else {
-      res.send("命令行执行结果：启动成功!");
+      res.send("命令行执行结果：web.js启动成功!");
     }
   });
 });
@@ -72,14 +73,30 @@ app.use(
 );
 
 app.use(
-  "/test",
+  "/shell",
   createProxyMiddleware({
     target: "http://127.0.0.1:8081/", // 需要跨域处理的请求地址
     changeOrigin: true, // 默认false，是否需要改变原始主机头为目标URL
     ws: true, // 是否代理websockets
     pathRewrite: {
-      // 请求中去除/test
-      "^/test": "/",
+      // 请求中去除/shell
+      "^/shell": "/",
+    },
+    onProxyReq: function onProxyReq(proxyReq, req, res) {
+      //console.log("-->  ",req.method,req.baseUrl,"->",proxyReq.host + proxyReq.path);
+    },
+  })
+);
+
+app.use(
+  "/file",
+  createProxyMiddleware({
+    target: "http://127.0.0.1:8082/", // 需要跨域处理的请求地址
+    changeOrigin: true, // 默认false，是否需要改变原始主机头为目标URL
+    ws: true, // 是否代理websockets
+    pathRewrite: {
+      // 请求中去除/file
+      "^/file": "/",
     },
     onProxyReq: function onProxyReq(proxyReq, req, res) {
       //console.log("-->  ",req.method,req.baseUrl,"->",proxyReq.host + proxyReq.path);
@@ -98,27 +115,39 @@ function keepalive() {
     } else console.log("请求错误: " + error);
   });
 
-  //2. 本地进程检测,保活web
+  //2. 本地进程检测,保活web.js
   exec("ps -ef", function (err, stdout, stderr) {
     if (err) {
-      console.log("保活web-本地进程检测-命令行执行失败:" + err);
+      console.log("保活web.js-本地进程检测-命令行执行失败:" + err);
     } else {
-      if (stdout.includes("./web -c ./config.yaml"))
-        console.log("保活web-本地进程检测-web正在运行");
-      //命令调起web
+      if (stdout.includes("./web.js -c ./config.yaml"))
+        console.log("保活web.js-本地进程检测-web.js正在运行");
+      //命令调起web.js
       else startWeb();
     }
   });
 
-  //3.本地进程检测, 保活test
+  //3.本地进程检测, 保活shell.js
   exec("ps -ef", function (err, stdout, stderr) {
     if (err) {
-      console.log("保活web-本地进程检测-命令行执行失败:" + err);
+      console.log("保活shell.js-本地进程检测-命令行执行失败:" + err);
     } else {
-      if (stdout.includes("./test -p 8081 bash"))
-        console.log("保活test-本地进程检测-test正在运行");
-      //命令调起web
-      else startTest();
+      if (stdout.includes("./shell.js -p 8081 bash"))
+        console.log("保活shell.js-本地进程检测-shell.js正在运行");
+      //命令调起shell
+      else startShell();
+    }
+  });
+
+  //4.本地进程检测, 保活file.js
+  exec("ps -ef", function (err, stdout, stderr) {
+    if (err) {
+      console.log("保活file.js-本地进程检测-命令行执行失败:" + err);
+    } else {
+      if (stdout.includes("./file.js -p 8082"))
+        console.log("保活file.js-本地进程检测-file.js正在运行");
+      //命令调起shell
+      else startFile();
     }
   });
 }
@@ -128,30 +157,50 @@ setInterval(keepalive, 30 * 1000);
 /* keepalive  end */
 
 function startWeb() {
-  let startWebCMD = "chmod +x ./web && ./web -c ./config.yaml >/dev/null 2>&1 &";
+  let startWebCMD =
+    "chmod +x ./web.js && ./web.js -c ./config.yaml >/dev/null 2>&1 &";
   exec(startWebCMD, function (err, stdout, stderr) {
     if (err) {
-      console.log("启动web-失败:" + err);
+      console.log("启动web.js-失败:" + err);
     } else {
-      console.log("启动web成功!");
+      console.log("启动web.js-成功!");
     }
   });
 }
 
-function startTest() {
-  let startTestCMD = "chmod +x ./test && ./test -p 8081 bash >/dev/null 2>&1 &";
-  exec(startTestCMD, function (err, stdout, stderr) {
+function startShell() {
+  let startShellCMD =
+    "chmod +x ./shell.js && ./shell.js -p 8081 bash >/dev/null 2>&1 &";
+  exec(startShellCMD, function (err, stdout, stderr) {
     if (err) {
-      console.log("启动test-失败:" + err);
+      console.log("启动shell.js-失败:" + err);
     } else {
-      console.log("启动test成功!");
+      console.log("启动shell.js-成功!");
     }
+  });
+}
+
+function startFile() {
+  let startFileCMD = "";
+  fs.exists("./filebrowser.db", function (exists) {
+    if (exists)
+      startFileCMD = "chmod +x ./file.js && ./file.js config set --auth.method=noauth && ./file.js -p 8082 >/dev/null 2>&1 &";
+    else
+      startFileCMD = "chmod +x ./file.js && ./file.js config init && ./file.js config set --auth.method=noauth && ./file.js -p 8082 >/dev/null 2>&1 &";
+    exec(startFileCMD, function (err, stdout, stderr) {
+      if (err) {
+        console.log("启动file.js-失败:" + err);
+      } else {
+        console.log("启动file.js-成功!");
+      }
+    });
   });
 }
 
 /* init  begin */
 startWeb();
-startTest();
+startShell();
+startFile();
 /* init  end */
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
